d4c715e08b76265a208ac5a422771194683ef3fc
Carlos <nashlasterby@gmail.com>
2022-09-15 10:46:44 -0300

    two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first-component/firstcomponent';
 import { ChildComponent } from './components/child/child.component';
 import { DetailsComponent } from './components/details/details.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { DetailsComponent } from './components/details/details.component';
     FirstComponent,
     ChildComponent,
     DetailsComponent,
+    TwoWayBindingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
